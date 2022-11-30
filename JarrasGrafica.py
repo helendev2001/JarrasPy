bc2e40f3ee928ad37ba2c0f7accac37991d3cc05
helendev2001 <hlmoreta@espe.edu.ec>
2022-11-29 23:30:55 -0500

    WORKING PROJECT

--- JarrasGrafica.py
+++ JarrasGrafica.py
@@ -2,120 +2,151 @@ from pygame import *
 import random
 import sys
 
-NEGRO = (0, 0, 0)
-ROJO = (255, 0, 0)
-CAFE = (90, 50, 15)
-CELESTE = (76, 226, 252)
-BLANCO = (255, 255, 255)
+# Colores
+NEGRO   = (0   , 0   , 0  )
+ROJO    = (255 , 0   , 0  )
+CAFE    = (90  , 50  , 15 )
+CELESTE = (76  , 226 , 252)
+BLANCO  = (255 , 255 , 255)
+AGUA    = (220 , 251 , 249)
 
-def crearBoton(pantalla, boton, mensaje, color, marcar,fuente):
+
+# Crea botones
+def crearBoton(pantalla, boton, mensaje, color, marcar, fuente):
     if boton.collidepoint(mouse.get_pos()):
         draw.rect(pantalla,  marcar, boton)
     else:
         draw.rect(pantalla, color, boton)
-    texto = fuente.render(mensaje, True, NEGRO)
-    pantalla.blit(texto, (boton.x+(boton.width-texto.get_width())/2, boton.y+(boton.height-texto.get_height())/2))
+    texto = fuente.render(mensaje, True, BLANCO)
+    pantalla.blit(texto, (boton.x+(boton.width-texto.get_width()) / 2, boton.y+(boton.height-texto.get_height())/2))
 
+
+# Parametros ventana pygame
 init()
-Letra = font.SysFont("Calibri", 20, bold=False, italic=False)
+Letra = font.SysFont("Times New Roman", 20, bold=False, italic=False)
 Dimensiones = (800, 500)
 Pantalla = display.set_mode(Dimensiones)
-display.set_caption("JARRAS")
+display.set_caption("Jarras Puzzle")
 
-x=150
-y=360
-ancho=75
-alto=30
-dist=25
-J5 = Rect(x, 100, 200, 250)
-J3 = Rect(y+dist, 200, 200, 150)
-BLlenar5 = Rect(x, y, ancho, alto)
-BVaciar5 = Rect(x+ancho+dist, y, ancho, alto)
-BLlenar3 = Rect(x+ancho+(7*dist), y, ancho, alto)
-BVaciar3 = Rect(x+ancho+(12*dist), y, ancho, alto)
-aux = 0
-clic = 0
+# Variables
+x = 100
+y = 100
+ancho = 100
+alto = 30
+escala = 50
+opcion = ""
+inicio = 0
+estadoJ1 = 0
+estadoJ2 = 0
+# Jarras
+J5 = Rect(x, y, 2*ancho, 5*escala)
+J3 = Rect(x+(2.5*y), 2*y, 2*ancho, 3*escala)
+
+# Botones
+BLlenar = Rect(Pantalla.get_width()-150, 100, ancho, alto)
+BVaciar = Rect(Pantalla.get_width()-150, 200, ancho, alto)
+BTraspasar = Rect(Pantalla.get_width()-150, 300, ancho, alto)
+incremento = y
 Terminar = False
 # Se define para poder gestionar cada cuando se ejecuta un fotograma
 reloj = time.Clock()
-
+v = 2
 while not Terminar:
 
     Pantalla.fill((255, 255, 255))
-    #Pantalla.fill(NEGRO)
+    # Pantalla.fill(NEGRO)
     # ---Manejo de eventos
     for Evento in event.get():
         if Evento.type == QUIT:
             Terminar = True
         if Evento.type == MOUSEBUTTONDOWN and Evento.button == 1:
-            if BLlenar5.collidepoint(mouse.get_pos()):
-                print("Llenar5")
-                aux = 1
-                clic = clic + 1
-            if BVaciar5.collidepoint(mouse.get_pos()):
-                print("Vaciar5")
-                aux = 2
-            if BLlenar3.collidepoint(mouse.get_pos()):
-                print("Llenar3")
-                aux = 3
-                clic = clic + 1
-                print(clic)
-            if BVaciar3.collidepoint(mouse.get_pos()):
-                print("Vaciar3")
-                aux = 4
             if J5.collidepoint(mouse.get_pos()):
-                print("Jarra de 5")
+                opcion = "Jarra5"
+                print("Selecciono Jarra de 5L")
+            if BLlenar.collidepoint(mouse.get_pos()) and opcion == "Jarra5":
+                incremento = 1;
+                opcion = "Llenar_Jarra5"
+                print("Llenar Jarra de 5L")
+            if BVaciar.collidepoint(mouse.get_pos()) and opcion == "Jarra5":
+                opcion = "Vaciar_Jarra5"
+                print("Vaciar Jarra de 5L")
             if J3.collidepoint(mouse.get_pos()):
-                print("Jarra de 3")
+                opcion = "Jarra3"
+                print("Selecciono Jarra de 3L")
+            if BLlenar.collidepoint(mouse.get_pos()) and opcion == "Jarra3":
+                incremento = 1;
+                opcion = "Llenar_Jarra3"
+                print("Llenar Jarra de 3L")
+            if BVaciar.collidepoint(mouse.get_pos()) and opcion == "Jarra3":
+                opcion = "Vaciar_Jarra3"
+                print("Vaciar Jarra de 3L")
 
-    if BLlenar5.collidepoint(mouse.get_pos()):
-        crearBoton(Pantalla,BLlenar5,"LLENAR",BLANCO,CELESTE,Letra)
-    else:
-        crearBoton(Pantalla, BLlenar5, "LLENAR", BLANCO, CELESTE, Letra)
 
-    if BLlenar3.collidepoint(mouse.get_pos()):
-        crearBoton(Pantalla, BLlenar3, "LLENAR", BLANCO, CELESTE, Letra)
-    else:
-        crearBoton(Pantalla, BLlenar3, "LLENAR", BLANCO, CELESTE, Letra)
-
-    if BVaciar5.collidepoint(mouse.get_pos()):
-        crearBoton(Pantalla, BVaciar5, "VACIAR", BLANCO, CELESTE, Letra)
-    else:
-        crearBoton(Pantalla, BVaciar5, "VACIAR", BLANCO, CELESTE, Letra)
-
-    if BVaciar3.collidepoint(mouse.get_pos()):
-        crearBoton(Pantalla, BVaciar3, "VACIAR", BLANCO, CELESTE, Letra)
-    else:
-        crearBoton(Pantalla, BVaciar3, "VACIAR", BLANCO, CELESTE, Letra)
-
+    crearBoton(Pantalla, BLlenar, "LLENAR", NEGRO, CELESTE, Letra)
+    crearBoton(Pantalla, BVaciar, "VACIAR", NEGRO, CELESTE, Letra)
+    crearBoton(Pantalla, BTraspasar, "TRASPASAR", NEGRO, CELESTE, Letra)
     # --Todos los dibujos van después de esta línea
-    draw.rect(Pantalla, ROJO, J5, 3)
-    draw.rect(Pantalla, ROJO, J3, 3)
+    draw.rect(Pantalla, NEGRO, J5, 3)
+    draw.rect(Pantalla, NEGRO, J3, 3)
 
-   # for row in range(297, 100, -50):
-        #draw.line(Pantalla, NEGRO, (120, row), (130, row), 3)
-    #for row2 in range(297, 200, -50):
-        #draw.line(Pantalla, NEGRO, (400, row2), (410, row2), 3)
+    if opcion == "Jarra5":
+        draw.rect(Pantalla, ROJO, J5, 3)
+    elif opcion == "Jarra3":
+        draw.rect(Pantalla, ROJO, J3, 3)
+     
+     
+    if (opcion == "Llenar_Jarra5" and not estadoJ2==1):          #print("Llenar Jarra de 5L")
+        if (incremento >= inicio and incremento < 5*escala):
+            draw.rect(Pantalla, AGUA, (x, y, 2*ancho, incremento))
+            draw.rect(Pantalla, NEGRO, J5, 3)
+            incremento += v
+            # print(incremento)
+        if (incremento >= 245):
+            draw.rect(Pantalla, AGUA, (x, y, 2*ancho, 5*escala))
+            draw.rect(Pantalla, NEGRO, J5, 3)
+        estadoJ1 = 1;
+    elif  (opcion == "Llenar_Jarra5" and estadoJ2==1):
+        if (incremento >= inicio and incremento < 5*escala):
+            draw.rect(Pantalla, AGUA, (x, y, 2*ancho, incremento))
+            draw.rect(Pantalla, NEGRO, J5, 3)
+            incremento += v
+            # print(incremento)
+        if (incremento >= 245):
+            draw.rect(Pantalla, AGUA, (x, y, 2*ancho, 5*escala))
+            draw.rect(Pantalla, NEGRO, J5, 3)
+            draw.rect(Pantalla, AGUA, (x+(2.5*y), 2*y, 2*ancho, 3*escala))
+            draw.rect(Pantalla, NEGRO, J3, 3)
 
-    if aux == 1:
-        draw.rect(Pantalla, CELESTE, J5, 0)
-        draw.rect(Pantalla, ROJO, J5, 3)
-    elif aux == 2:
-        draw.rect(Pantalla, NEGRO, J5, 0)
-        draw.rect(Pantalla, ROJO, J5, 3)
-    elif aux == 3:
-        draw.rect(Pantalla, CELESTE, J3, 0)
-        draw.rect(Pantalla, ROJO, J3, 3)
-    elif aux == 4:
-        draw.rect(Pantalla, NEGRO, J3, 0)
-        draw.rect(Pantalla, ROJO, J3, 3)
-    elif (clic%2)==0:
-        draw.rect(Pantalla, CELESTE, J5, 0)
-        draw.rect(Pantalla, ROJO, J5, 3)
-        draw.rect(Pantalla, CELESTE, J3, 0)
-        draw.rect(Pantalla, ROJO, J3, 3)
+        estadoJ1 = 1;
+        estadoJ2 = 1;
 
+    if (opcion == "Llenar_Jarra3" and not estadoJ1==1):
+        #print("Llenar Jarra de 5L")
+        if (incremento >= inicio and incremento < 3*escala):
+            draw.rect(Pantalla, AGUA, (x+(2.5*y), 2*y, 2*ancho, incremento))
+            draw.rect(Pantalla, NEGRO, J3, 3)
+            incremento += v
+            # print(incremento)
+        if (incremento >= 145):
+            draw.rect(Pantalla, AGUA, (x+(2.5*y), 2*y, 2*ancho, 3*escala))
+            draw.rect(Pantalla, NEGRO, J3, 3)
+        estadoJ2 = 1;
+    elif (opcion == "Llenar_Jarra3" and estadoJ1==1):
+        if (incremento >= inicio and incremento < 3*escala):
+            draw.rect(Pantalla, AGUA, (x+(2.5*y), 2*y, 2*ancho, incremento))
+            draw.rect(Pantalla, NEGRO, J3, 3)
+            incremento += v
+            # print(incremento)
+        if (incremento >= 245):
+            draw.rect(Pantalla, AGUA, (x+(2.5*y), 2*y, 2*ancho, 3*escala))
+            draw.rect(Pantalla, NEGRO, J3, 3)
+            draw.rect(Pantalla, AGUA, (x, y, 2*ancho, 5*escala))
+            draw.rect(Pantalla, NEGRO, J5, 3)
+        estadoJ1 = 1;
+        estadoJ2 = 1;
+    # elif opcion==opcion="Llenar Jarra de 5L"
+    print(estadoJ1,estadoJ2)
     # --Todos los dibujos van antes de esta línea
     display.flip()
-    reloj.tick(20)  # Limitamos a 20 fotogramas por segundo
-pygame.qu
+    reloj.tick(60)  # Limitamos a 20 fotogramas por segundo
+
